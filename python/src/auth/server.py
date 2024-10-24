8b26acd94c2ef8773d9233cf76c76f19a8922f58
DouglasL7 <lopesdouglas92@gmail.com>
2024-10-23 21:17:16 -0300

    Auth service code, almost finished

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,9 +1,45 @@
 import jwt, datetime, os
+import mysql.connector
 from flask import Flask, request
-from flask_mysqldb import MySQL
+
 
 server = Flask(__name__)
-mysql = MySQL(server)
 
-# config
-server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+db_config = {
+    "host": os.getenv("MYSQL_HOST", "localhost"),
+    "user": os.getenv("MYSQL_USER", "auth_user@localhost"),
+    "password": os.getenv("MYSQL_PASSWORD", "Admin123"),
+    "database": os.getenv("MYSQL_DB", "auth"),
+}
+
+
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
+
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "Missing credentials", 401
+
+    # check db for username and password
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    res = cursor.execute(
+        "SELECT email, password FROM user WHERE email=%s", (auth.username)
+    )
+
+    if res > 0:
+        user_row = cursor.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "Invalid credentials", 401
+        else:
+            return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+
+    else:
+        return "Invalid credentials", 401
